bb7140755ab513600ab2a1b3dc4e6631feaf2260
MICHA£ SOKO£OWSKI <programming.email.acct@gmail.com>
2025-08-30 15:09:16 +0200

    WIP -MissionId

--- src/main/java/org/gitsokolek/spacex/mission/model/MissionId.java
+++ src/main/java/org/gitsokolek/spacex/mission/model/MissionId.java
@@ -1,59 +1,33 @@
 package org.gitsokolek.spacex.mission.model;
 
-import org.gitsokolek.spacex.utilities.developmentutilities.DevUtils;
-
 import java.io.Serializable;
 import java.util.Objects;
 import java.util.UUID;
 
-public final class MissionId implements Serializable
-{
+public final class MissionId implements Serializable {
 	private final UUID value;
 
-	private static final String NOT_IMPLEMENTED_YET = DevUtils.notImplementedYet();
-
-
-
-	public MissionId(UUID value)
-	{
-		throw new UnsupportedOperationException(NOT_IMPLEMENTED_YET);
+	public MissionId(UUID value) {
+		this.value = Objects.requireNonNull(value);
 	}
 
-
-
-	public static MissionId random()
-	{
-		throw new UnsupportedOperationException(NOT_IMPLEMENTED_YET);
+	public static MissionId random() {
+		return new MissionId(UUID.randomUUID());
 	}
 
-
-
-	public UUID value()
-	{
-		return value;
-	}
-
-
+	public UUID value() { return value; }
 
 	@Override
-	public boolean equals(Object o)
-	{
-		throw new UnsupportedOperationException(NOT_IMPLEMENTED_YET);
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof MissionId)) return false;
+		MissionId missionId = (MissionId) o;
+		return value.equals(missionId.value);
 	}
-
-
 
 	@Override
-	public int hashCode()
-	{
-		throw new UnsupportedOperationException(NOT_IMPLEMENTED_YET);
-	}
-
-
+	public int hashCode() { return value.hashCode(); }
 
 	@Override
-	public String toString()
-	{
-		throw new UnsupportedOperationException(NOT_IMPLEMENTED_YET);
-	}
+	public String toString() { return value.toString(); }
 }
